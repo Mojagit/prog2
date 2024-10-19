34d2463f439b5099c0c2fef4569d5372a6925273
Anton Ahlzén <anton.ahlzen@gmail.com>
2024-10-19 19:25:42 +0200

    added plots and updated 1.1 and 1.2

--- MA4/MA4_files/MA4_files/MA4_1_1.py
+++ MA4/MA4_files/MA4_files/MA4_1_1.py
@@ -1,24 +1,50 @@
-
 """
 Solutions to module 4
 Review date:
 """
 
-student = ""
+student = "Anton Ahlzén"
 reviewer = ""
 
-
+import math
 import random as r
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
 
 def approximate_pi(n):
     # Write your code here
-    return
-    
+    n_c_xlist = []
+    n_c_ylist = []
+    n_xlist = []
+    n_ylist = []
+
+    n_c = 0
+    for point in range(n):
+        x = r.uniform(-1, 1)
+        y = r.uniform(-1, 1)
+        if x ** 2 + y ** 2 <= 1.0:
+            n_c += 1
+            n_c_xlist.append(x)
+            n_c_ylist.append(y)
+        else:
+            n_xlist.append(x)
+            n_ylist.append(y)
+    plt.plot(n_c_xlist, n_c_ylist, 'ro', )
+    plt.plot(n_xlist, n_ylist, 'bo')
+    plt.savefig(f"pi_{n}.png")
+    plt.clf()
+
+    pi_approx = 4 * n_c / n
+    print(pi_approx)
+    print(math.pi)
+    return pi_approx
+
+
 def main():
     dots = [1000, 10000, 100000]
     for n in dots:
         approximate_pi(n)
 
+
 if __name__ == '__main__':
-	main()
+    main()
--- MA4/MA4_files/MA4_files/MA4_1_2.py
+++ MA4/MA4_files/MA4_files/MA4_1_2.py
@@ -1,29 +1,57 @@
-
 """
 Solutions to module 4
 Review date:
 """
 
-student = ""
+student = "Anton Ahlzén"
 reviewer = ""
 
+
+import math
 import math as m
 import random as r
+import functools
 
-def sphere_volume(n, d):
-    # n is a list of set of coordinates
-    # d is the number of dimensions of the sphere 
 
-    return 
+def sphere_volume(n, dimensions):
+    # n is the number of coordinates
+    # d is the number of dimensions of the sphere
 
-def hypersphere_exact(n,d):
-    return 
-     
+    # distances = [(functools.reduce(lambda x,y: x+y, map(lambda x: x**2, [r.uniform(-R/2, R/2) for d in range(dimensions)]))) for point in range(n)]
+
+    squared_distances = []
+    for _ in range(n):
+        point = [r.uniform(-1, 1) for dim in range(dimensions)]
+        point_sqrd = list(map(lambda x: x**2, point))
+        distance_squared = functools.reduce(lambda x,y: x+y, point_sqrd)
+        squared_distances.append(distance_squared)
+
+    n_c = len(list(filter(lambda x: x <= 1, squared_distances)))
+    volume = (n_c * 2**dimensions) / n
+    return volume
+
+
+def hypersphere_exact(n, d):
+    return math.pi**(d/2) / math.gamma(d/2 + 1)
+
+
 def main():
     n = 100000
-    d = 2
-    sphere_volume(n,d)
+    d = 3
+    approx_2d = sphere_volume(n, d)
+    exact_2d = hypersphere_exact(n, d)
+    print('Approximation for 2d: ', approx_2d)
+    print('Exact for 2d: ', exact_2d)
+    print('Fraction: ', exact_2d / approx_2d)
+
+    print()
+    d = 11
+    approx_11d = sphere_volume(n, d)
+    exact_11d = hypersphere_exact(n, d)
+    print('Approximation for 11d: ', approx_11d)
+    print('Exact for 11d: ', exact_11d)
+    print('Fraction: ', exact_11d / approx_11d)
 
 
 if __name__ == '__main__':
-	main()
+    main()
